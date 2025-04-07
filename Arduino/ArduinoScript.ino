f587deeb57e4af4cf6d90e874679a221d61f67ac
srizz03 <guyfarting722@gmail.com>
2025-04-07 02:06:31 -0400

    Update ArduinoScript.ino
    larger update for bool flags and encoder control NOT TESTED, DONT UPLOAD

--- Arduino/ArduinoScript.ino
+++ Arduino/ArduinoScript.ino
@@ -2,11 +2,19 @@
 const int serialBaudRate = 9600;
 
 //Setting the values for the motor driver control pins nad the relay control pin
-const int motorLeft = 8;  //TO IN1 **CHANGE
-const int motorRight = 12;  //TO IN4 **CHANGE
+const int motorLeft = 8;  //TO DIR
+const int motorRight = 12;  //TO DIR
 const int speedLeft = 10;  //TO ENB
 const int speedRight = 11;  //TO ENA
 const int augerCont = 9;
+const int leftEncoderPin = 2;   // Must be interrupt-capable
+const int rightEncoderPin = 3;  // Must be interrupt-capable
+volatile long leftECount = 0;
+volatile long rightECount = 0;
+long prevLeftEncoder = 0;
+long prevRightEncoder = 0;
+const int baseSpeed = 150;  //Default speed of motors before encoder adjustment, only when encoders are used
+float Kp = 1.0;  //Proportional gain of encoder controller
 
 //Setting the values for the motor driver queues **TROUBLESHOOT HERE**
 const String forward = "Forward";
@@ -21,12 +29,18 @@ const int refreshRate = 200;  //In miliseconds, has min of 50, max of 1000 (sens
 //Setting the values for the ultrasonic sensor control pins
 const int trigLeft = 6;
 const int echoLeft = 7;
-const int trigRight = 2;
-const int echoRight = 3;
+const int trigRight = 2;  //**ENCODER CONFLICT
+const int echoRight = 3;  //**ENCODER CONFLICT
 const int trigRear = 4;
 const int echoRear = 5;
 int rSpeed = 0;
 int lSpeed = 0;
+
+//Debugging flags
+const bool takeSonicData = false;
+const bool listenForCommands = true;
+const bool useMotorEncoders = false;
+const bool isEStop = false;
 
 void setup() {
     //Setting the baud rate
@@ -40,6 +54,13 @@ void setup() {
     pinMode(speedLeft, OUTPUT);
     pinMode(speedRight, OUTPUT);
 
+    if (useMotorEncoders) {
+        pinMode(leftEncoderPin, INPUT_PULLUP);
+        pinMode(rightEncoderPin, INPUT_PULLUP);
+        attachInterrupt(digitalPinToInterrupt(leftEncoderPin), leftEncoderISR, RISING);
+        attachInterrupt(digitalPinToInterrupt(rightEncoderPin), rightEncoderISR, RISING);
+    }
+
     //Setting the ultrasonic sensor control pins
     pinMode(trigLeft, OUTPUT);
     pinMode(echoLeft, INPUT);
@@ -47,8 +68,6 @@ void setup() {
     pinMode(echoRight, INPUT);
     pinMode(trigRear, OUTPUT);
     pinMode(echoRear, INPUT);
-
-    analogWrite(speedLeft, lSpeed);
 }
 
 long readUltrasonic(int trigPin, int echoPin) {
@@ -63,54 +82,95 @@ long readUltrasonic(int trigPin, int echoPin) {
     return distance;
 }
 
-void loop() {
-    
-    String distLeft = String(readUltrasonic(trigLeft, echoLeft));
-    String distRight = String(readUltrasonic(trigRight, echoRight));
-    String distRear = String(readUltrasonic(trigRear, echoRear));
-    Serial.println("R" + distRight);
-    Serial.println("L" + distLeft);
-    Serial.println("B" + distRear);
-    
-    if (Serial.available()>0) {
-        String command = Serial.readStringUntil('\n');
+void leftEncoderISR() {
+  leftEncoderCount++;
+}
 
-        //FORWARD
-        if (command == forward) {  // FORWARD
+void rightEncoderISR() {
+  rightEncoderCount++;
+}
+
+void balanceMotors() {
+  long leftDelta = leftEncoderCount - prevLeftEncoder;
+  long rightDelta = rightEncoderCount - prevRightEncoder;
+
+  int error = leftDelta - rightDelta;
+  int adjustment = Kp * error;
+
+  int rightSpeed = baseSpeed + adjustment;
+  rightSpeed = constrain(rightSpeed, 0, 255); // keep in PWM range
+
+  analogWrite(speedLeft, baseSpeed);
+  analogWrite(speedRight, rightSpeed);
+
+  prevLeftEncoder = leftEncoderCount;
+  prevRightEncoder = rightEncoderCount;
+}
+
+void listenSerial() {
+    if (Serial.available()>0) {
+        //Read the command
+        String command = Serial.readStringUntil('\n');
+        
+        //Motor Control
+        if (command == forward) {
             digitalWrite(motorLeft, LOW);
-            digitalWrite(motorRight, LOW); // M2 Forward
-            analogWrite(speedLeft, lSpeed); // M1 Speed
-            analogWrite(speedRight, rSpeed); // M2 Speed
+            digitalWrite(motorRight, LOW);
+            
+            if (useMotorEncoders)
+                balanceSpeed();
+            else {
+                analogWrite(speedLeft, baseSpeed);
+                analogWrite(speedRight, baseSpeed);
+            }
         } 
-        //STOP
-        else if (command == allStop) {  // STOP
+        else if (command == allStop) {
             digitalWrite(motorLeft, LOW);
-            digitalWrite(motorRight, LOW); // M2 OFF
-            analogWrite(speedLeft, 0);
-            analogWrite(speedRight, 0);
+            digitalWrite(motorRight, LOW);
+
+            if (useMotorEncoders)
+                balanceSpeed();
+            else {
+                analogWrite(speedLeft, 0);
+                analogWrite(speedRight, 0);
+            }
+            
             digitalWrite(augerCont, LOW);
         }
-        //TURN RIGHT
-        else if (command == rightTurn) {  // RIGHT TURN
+        else if (command == rightTurn) {  //Right
             digitalWrite(motorLeft, HIGH);
-            digitalWrite(motorRight, LOW); // M2 Forward, turn right
-            analogWrite(speedLeft, lSpeed); // M1 Speed
-            analogWrite(speedRight, lSpeed); // M2 Speed
+            digitalWrite(motorRight, LOW);
+
+            if (useMotorEncoders)
+                balanceSpeed();
+            else {
+                analogWrite(speedLeft, baseSpeed);
+                analogWrite(speedRight, baseSpeed);
+            }
         }
-        //TURN LEFT
-        else if (command == leftTurn) {  // LEFT TURN
+        else if (command == leftTurn) {  //Left
             digitalWrite(motorLeft, LOW);
-            digitalWrite(motorRight, HIGH); // M2 Reverse, turn left
-            analogWrite(speedLeft, lSpeed); // M1 Speed
-            analogWrite(speedRight, rSpeed); // M2 Speed
+            digitalWrite(motorRight, HIGH);
+
+            if (useMotorEncoders)
+                balanceSpeed();
+            else {
+                analogWrite(speedLeft, baseSpeed);
+                analogWrite(speedRight, baseSpeed);
+            }
         }
-        //BACKWARDS
-        else if (command == backward) {  // BACKWARDS
+        else if (command == backward) {
             digitalWrite(motorLeft, HIGH);
-            digitalWrite(motorRight, HIGH); // M2 Reverse
-            analogWrite(speedLeft, lSpeed); // M1 Speed
-            analogWrite(speedRight, rSpeed); // M2 Speed
+            digitalWrite(motorRight, HIGH);
+
+            if (useMotorEncoders)
+                balanceSpeed();
+            else {
+                analogWrite(speedLeft, baseSpeed);
+                analogWrite(speedRight, baseSpeed);
+            }
         }
+            
         //Auger Control
         else if (command == augerStart) {
             digitalWrite(augerCont, HIGH);
@@ -118,22 +178,41 @@ void loop() {
         else if (command == augerStart) {
             digitalWrite(augerCont, LOW);
         }
+            
         //Speed Control
         else if (command == "25"){
-          rSpeed = lSpeed = 64;
+          rSpeed = lSpeed = baseSpeed = 64;
         }
         else if (command == "50"){
-          rSpeed = lSpeed = 130;
+          rSpeed = lSpeed = baseSpeed = 130;
         }
         else if (command == "75"){
-          rSpeed = lSpeed = 190;
+          rSpeed = lSpeed = baseSpeed = 190;
         }
         else if (command == "100"){
-          rSpeed = lSpeed = 250;
+          rSpeed = lSpeed = baseSpeed = 250;
         }
         else {
-            
+            //Do nothing
         } 
     }
+}
+
+void sendUltrasonic() {
+    String distLeft = String(readUltrasonic(trigLeft, echoLeft));
+    String distRight = String(readUltrasonic(trigRight, echoRight));
+    String distRear = String(readUltrasonic(trigRear, echoRear));
+    Serial.println("R" + distRight);
+    Serial.println("L" + distLeft);
+    Serial.println("B" + distRear);
+}
+
+void loop() {
+    if (takeSonicData)
+        sendUltrasonic();
+    
+    if (listenForCommand)
+        listenSerial();
+
     delay(refreshRate);
 }
