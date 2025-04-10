142bbd78da82c775a285dea71fa767ace465f9af
srizz03 <guyfarting722@gmail.com>
2025-04-10 12:01:38 -0400

    Update ArduinoScript.ino
    voltage monitor functionality

--- Arduino/ArduinoScript.ino
+++ Arduino/ArduinoScript.ino
@@ -1,10 +1,28 @@
 //Setting the values for serial communication
 const int serialBaudRate = 9600;
 const int eStopPin = 13;
+int counter = 0;
+
+//Setting the values for the battery monitor pins
+const int battery12VPin = A0;
+const int battery40VPin = A1;
+const float voltageReference = 5.0; // Arduino ADC reference (5V for most boards)
+const int adcMax = 1023;            // 10-bit ADC resolution
+const double nom40V = 40;
+const double cut40V = 33;
+const double res40Vtop = 91180;  //So about 2.009V to 1.658V
+const double res40Vbot = 4823.5;
+const double nom12V = 12.8;
+const double cut12V = 10;
+const double res12Vtop = 40398;  //So abaout 4.24V to 3.31V
+const double res12Vbot = 20003;
+const double divRatio40V = res40Vbot / (res40Vtop + res40Vbot);
+const double divRatio12V = res12Vbot / (res12Vtop + res12Vbot);
+
 
 //Setting the values for the motor driver control pins nad the relay control pin
-const int motorLeft = 8;  //TO DIR
-const int motorRight = 12;  //TO DIR
+const int motorLeft = 12;  //TO DIR
+const int motorRight = 8;  //TO DIR
 const int speedLeft = 10;  //TO ENB
 const int speedRight = 11;  //TO ENA
 const int augerCont = 9;
@@ -27,8 +45,9 @@ const String augerStart = "GoAug";
 const String augerStop = "CeaseAug";
 const String recEStop = "JetEStop";
 const String sendEStop = "ArdEStop";
-const String allClear = "AllClearAllClear";
-const int refreshRate = 200;  //In miliseconds, has min of 50, max of 1000 (sensor limits)
+const String recClear = "JetAllClear";
+const String sendClear = "ArdAllClear";
+const int refreshRate = 2000;  //In miliseconds, has min of 50, max of 1000 (sensor limits), nominal 200
 
 //Setting the values for the ultrasonic sensor control pins
 const int trigLeft = 6;
@@ -49,7 +68,7 @@ const bool isEStop = false;
 void setup() {
     //Setting the baud rate
     Serial.begin(serialBaudRate);
-    pinMode(eStopPin, INPUT);
+    pinMode(eStopPin, INPUT_PULLUP);
 
     //Setting the motor driver control pins
     pinMode(motorLeft, OUTPUT);
@@ -178,7 +197,7 @@ void listenSerial() {
         else if (command == augerStart) {
             digitalWrite(augerCont, HIGH);
         }
-        else if (command == augerStart) {
+        else if (command == augerStop) {
             digitalWrite(augerCont, LOW);
         }
             
@@ -204,12 +223,13 @@ void listenSerial() {
             digitalWrite(motorRight, LOW);
             digitalWrite(augerCont, LOW);
 
-            while(1) {  //Freeze until all clear si recived
+            while(1) {  //Freeze until all clear is recived
                 if (Serial.available() > 0) {
                     String command = Serial.readStringUntil('\n');
-                    if (command == allClear)
+                    if (command == recClear)
                         break;
                 }
+                delay(refreshRate);
             }
         }
 
@@ -228,8 +248,39 @@ void sendUltrasonic() {
     Serial.println("B" + distRear);
 }
 
+void checkBattLevels() {
+    int raw12V = analogRead(battery12VPin);
+    int raw40V = analogRead(battery40VPin);
+    float voltage12V = (raw12V / (float)adcMax) * voltageReference;
+    float voltage40V = (raw40V / (float)adcMax) * voltageReference;
+
+    float actual12V = voltage12V / divRatio12V;
+    float actual40V = voltage40V / divRatio40V;
+
+    int per12V = (int)(100 * (actual12V - cut12V) / (nom12V - cut12V));
+    int per40V = (int)(100 * (actual40V - cut40V) / (nom40V - cut40V));
+
+    per12V = constrain(per12V, 0, 100);
+    per40V = constrain(per40V, 0, 100);
+
+    /*
+    Serial.print("Raw 12V ADC: ");
+    Serial.println(raw12V);
+    Serial.print("Raw 40V ADC: ");
+    Serial.println(raw40V);
+
+    Serial.print("Voltage 12V: ");
+    Serial.println(voltage12V, 2);
+    Serial.print("Voltage 40V: ");
+    Serial.println(voltage40V, 2);
+    */
+
+    Serial.println("P" + String(per12V));
+    Serial.println("Q" + String(per40V));
+}
+
 void checkEStop() {
-    if(digitalRead(eStopPin) == LOW) {  //E-Stop triggered
+    if (digitalRead(eStopPin) == LOW) {  //E-Stop triggered
         analogWrite(speedLeft, 0);
         analogWrite(speedRight, 0);
         digitalWrite(motorLeft, LOW);
@@ -238,13 +289,16 @@ void checkEStop() {
 
         Serial.println(sendEStop);
         
-        while(digitalRead(eStopPin) == LOW) {
-            if(digitalRead(eStopPin) == HIGH){
-
+        while (1) {
+            if (digitalRead(eStopPin) == HIGH) {
+                Serial.println(sendClear);
+                break;
             }
+            delay(refreshRate);
         }
     } 
 }
+
 void loop() {
     if (takeSonicData)
         sendUltrasonic();
@@ -252,6 +306,8 @@ void loop() {
     if (listenForCommands)
         listenSerial();
 
+    checkBattLevels();
     checkEStop();
+    Serial.println(counter++);
     delay(refreshRate);
 }
