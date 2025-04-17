328872c474ce1e800bb280df1db619ae5e57fc5c
LucasPeterson <lpeterson0071@gmail.com>
2025-04-17 12:14:49 -0400

    Update start_ros.sh

--- start_ros.sh
+++ start_ros.sh
@@ -22,15 +22,15 @@ sleep 5
 ROS_NAMESPACE=/stereo_camera rosrun stereo_image_proc stereo_image_proc &
 sleep 5
 
-#rosrun tf static_transform_publisher 0.4445 0.1143 0.3556 -1.5708 0 -1.5708 base_link camera_link 100 
-#sleep 1
+rosrun tf static_transform_publisher 0.4445 0.1143 0.3556 -1.5708 0 -1.5708 base_link camera_link 100 &
+sleep 1
 
-#rosrun tf2_ros static_transform_publisher 0 -0.03 0 0 0 0 1 camera_link left_camera 
-#sleep 1
+rosrun tf2_ros static_transform_publisher 0 -0.03 0 0 0 0 1 camera_link left_camera &
+sleep 1
 
-#rosrun tf2_ros static_transform_publisher 0 0.03 0 0 0 0 1 camera_link right_camera 
-#sleep 1
+rosrun tf2_ros static_transform_publisher 0 0.03 0 0 0 0 1 camera_link right_camera &
+sleep 1
 
-#rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 1 camera_link camera 
-#sleep 1
+rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 1 camera_link camera 
+sleep 1
 
